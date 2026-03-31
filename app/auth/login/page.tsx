c4832513df208ce237d9fb60b592b5c46e2d42e5
Aliaksei-Varabyou <Aliaksei_Varabyou@epam.com>
2026-03-31 17:41:36 +0200

    feat: login page

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,4 +1,38 @@
+'use client';
 
-export default function Page() {
-  return <div>Coming soon</div>;
+import { useState } from "react";
+import { signIn } from "next-auth/react";
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    await signIn("credentials", {
+      email,
+      password,
+      redirect: true,
+      callbackUrl: '/dashboard'
+    })
+  }
+
+  return (
+    <div className="space-y-4">
+      <input 
+        className="border p-2"
+        name="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input 
+        className="border p-2"
+        name="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button className="border px-4 py-2" onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
+
 }
